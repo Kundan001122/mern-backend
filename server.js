3893e741c53af8a66ec97307769008e5370f6e02
Kundan Kumar Mandal <kundankumar6204340@gmail.com>
2025-05-25 11:34:02 +0530

    Updated backend

--- server.js
+++ server.js
@@ -14,11 +14,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // ******************Middleware
-app.use(cors({
-    origin: "https://schoolproject32.netlify.app/", // Netlify site URL
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
+
+// app.use(cors({
+//     origin: "https://schoolproject32.netlify.app/", // Netlify site URL
+//     credentials: true
+// }));
 
 // ******************Routes
 app.use("/api/users", userRoutes);
